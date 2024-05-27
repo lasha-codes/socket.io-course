e9e6ff4c9f234d8d7dee0f2b58091dcac87827c8
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-05-27 16:39:27 +0400

    learning socket.io but by different aproaches now

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,74 +1,50 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-/* eslint-disable no-unused-vars */
-import { useEffect, useState } from 'react'
-import io from 'socket.io-client'
-import Input from './components/input'
+import { useState } from 'react'
 
 const App = () => {
-  const [score, setScore] = useState({})
-  const [allScores, setAllScores] = useState([])
-  const socket = io('localhost:3000')
+  const [formInputs, setFormInputs] = useState({})
+  const handleInput = (event) => {
+    const { name, value } = event.target
 
-  function connectSocket() {
-    socket.on('connection', (socket) => {})
+    let obj = { [name]: value }
+
+    setFormInputs((prev) => ({ ...prev, ...obj }))
   }
 
-  function handleInput(event) {
-    let { name, value } = event.target
-    let currentObj = { [name]: value }
-
-    setScore((prev) => ({ ...prev, ...currentObj }))
+  const handleSubmit = () => {
+    console.log(formInputs)
   }
 
-  function sendScores() {
-    socket.emit('scores', score)
-    socket.on('playerScores', (scores) => {
-      setAllScores(scores)
-    })
-  }
-
-  useEffect(() => {
-    connectSocket()
-  }, [connectSocket])
   return (
-    <div className='w-full h-screen flex items-center justify-center text-white bg-zinc-800'>
-      <div className='flex flex-col items-center gap-4'>
-        <h1 className='text-3xl font-medium mb-5'>
-          React Multiplayer Dashboard
-        </h1>
-        <Input
-          name='name'
-          handleInput={handleInput}
-          placeholder='Enter your name'
-        />
-        <Input
-          name='score'
-          handleInput={handleInput}
-          placeholder='Enter your Score'
-        />
-        <button
-          className='mt-[0.5rem] bg-black text-white px-5 py-2 rounded hover:bg-zinc-900 transition-all duration-300 ease-in-out'
-          onClick={sendScores}
-        >
-          Publish Score
-        </button>
+    <div className='w-full h-screen flex items-center justify-center bg-zinc-800 text-white'>
+      <div className='flex flex-col items-center gap-10'>
+        <h1 className='font-medium text-4xl'>Crud operations</h1>
+        <div className='flex flex-col gap-3 items-center'>
+          <input
+            onChange={handleInput}
+            name='name'
+            placeholder='Enter your name'
+            className='py-1 rounded pl-[1rem] w-[15rem] bg-zinc-700 border-white outline-none'
+          />
+          <input
+            onChange={handleInput}
+            name='age'
+            placeholder='Enter your age'
+            className='py-1 rounded pl-[1rem] w-[15rem] bg-zinc-700 border-white outline-none'
+          />
+          <input
+            onChange={handleInput}
+            name='phone'
+            placeholder='Enter your Phone Number'
+            className='py-1 rounded pl-[1rem] w-[15rem] bg-zinc-700 border-white outline-none'
+          />
 
-        <table className='w-full'>
-          <tbody>
-            <tr>
-              <th className='border border-white p-2'>Name</th>
-              <th className='border border-white p-2'>Score</th>
-            </tr>
-            {allScores.map((score, idx) => {
-              return (
-                <tr key={idx}>
-                  <td className='border border-white p-2'>{score?.name}</td>
-                  <td className='border border-white p-2'>{score?.score}</td>
-                </tr>
-              )
-            })}
-          </tbody>
-        </table>
+          <button
+            onClick={handleSubmit}
+            className='bg-black hover:opacity-70 px-5 py-1 rounded-md transition-all duration-300 ease-in-out'
+          >
+            Add Data
+          </button>
+        </div>
       </div>
     </div>
   )
